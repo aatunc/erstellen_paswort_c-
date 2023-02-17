4ac69b18854552791128fab1583476622a003c69
A Tunc <106016458+aatunc@users.noreply.github.com>
2023-02-17 14:23:44 +0100

    Projektdateien hinzufügen.

---
+++ erstellen_paswort_c++/erstellen_paswort_c++.cpp
@@ -0,0 +1,39 @@
+// erstellen_paswort_c++.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
+//
+
+#include <iostream>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	int i = 0;
+	int zeichenwert = 0;
+	int zahlen = 0;
+	string passwort = "";
+	srand(time(0));
+
+	cout << "paswort generieren";
+	
+	for (i = 1; i <= 8; i++) {
+
+		zeichenwert = rand() % 26;
+		zahlen = rand() % 26;
+		passwort += char(65 + zeichenwert + zahlen);
+
+	}
+	cout << "passwort: " << passwort;
+}
+
+// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
+// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
+
+// Tipps für den Einstieg: 
+//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
+//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
+//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
+//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
+//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
+//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
